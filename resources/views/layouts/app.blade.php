2950205a6de663e3f019303fcb1de3b3b319aa74
Mark Angelo Tumbokon <mark.tumbokon@sumofy.me>
2019-04-11 09:43:00 +0800

    hide navbar seach (not functional yet)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,7 +143,7 @@
 
                 @if (!empty($user->type) && $user->type == 'admin')
                     <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                    <!-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
                             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
@@ -152,7 +152,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form> -->
                 @endif
 
                 <!-- Topbar Navbar -->
@@ -160,11 +160,10 @@
 
                     @if (!empty($user->type) && $user->type == 'admin')
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
+                        <!-- <li class="nav-item dropdown no-arrow d-sm-none">
                             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
                             </a>
-                            <!-- Dropdown - Messages -->
                             <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
@@ -177,7 +176,7 @@
                                     </div>
                                 </form>
                             </div>
-                        </li>
+                        </li> -->
                     @endif
 
                     @if (!empty($alerts))
